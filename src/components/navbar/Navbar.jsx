eff6d34ab9e54983c8ccde88254faeaae50ad38d
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-02 13:01:02 +0200

    Rendering Explained Done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
 
@@ -44,6 +46,13 @@ const Navbar = () => {
 						{link.title}
 					</Link>
 				))}
+				<button
+					onClick={() => {
+						console.log('logged out');
+					}}
+				>
+					Logout
+				</button>
 			</div>
 		</div>
 	);
