bb08c475e44ea6fa2bacb092f82d9048595998a9
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-04 21:35:02 +0200

    How To Fetch Data On The Client Side Done

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import useSWR from 'swr';
 import styles from './page.module.css';
 
 const Dashboard = () => {
+	// const [data, setDate] = useState([]);
+	// const [err, setErr] = useState(false);
+	// const [isLoading, setIsLoading] = useState(false);
+
+	// useEffect(() => {
+	// 	const getDate = async () => {
+	// 		setIsLoading(true);
+	// 		const res = await fetch('http://localhost:3000/api/posts', {
+	// 			cache: 'no-store',
+	// 		});
+
+	// 		if (!res.ok) {
+	// 			setErr(true);
+	// 		}
+
+	// 		const data = await res.json();
+
+	// 		setDate(data);
+	// 		setIsLoading(false);
+	// 	};
+
+	// 	getDate();
+	// }, []);
+
+	const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+	const { data, mutate, error, isLoading } = useSWR(`/api/posts?username=${session?.data?.user.name}`, fetcher);
+
 	return <div className={styles.container}>Dashboard</div>;
 };
 
