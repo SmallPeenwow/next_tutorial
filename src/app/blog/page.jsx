b5e57938a96541b6dd06b28a911e5edb982cfd81
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-05 12:22:51 +0200

    API Folder And CRUD Operations Done

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -8,12 +8,10 @@ async function getData() {
 	const res = await fetch('http://localhost:3000/api/posts', {
 		cache: 'no-store',
 	});
-
 	if (!res.ok) {
 		throw new Error('Failed to fetch data');
 		// return notFound();
 	}
-
 	return res.json();
 }
 
--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connect = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO);
+		await mongoose.connect(process.env.NEXT_PUBLIC_MONGO);
 	} catch (error) {
 		throw new Error('connection failed.');
 	}
