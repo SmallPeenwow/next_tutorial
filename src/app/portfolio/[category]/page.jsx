37f0d097cc47be1852705652ddee746ddf55647f
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-05 08:48:25 +0200

    How To Fetch Data From Local Json File Done

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 import styles from './page.module.css';
 import Button from '@/components/button/Button';
 import Image from 'next/image';
+import { items } from './data.js';
+import { notFound } from 'next/navigation';
+
+const getData = (cat) => {
+	const data = items[cat];
+
+	if (data) {
+		return data;
+	}
+
+	return notFound();
+};
 
 const Category = ({ params }) => {
+	const data = getData(params.category);
+
 	return (
 		<div className={styles.container}>
 			<h1 className={styles.catTitle}>{params.category}</h1>
